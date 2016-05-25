15fa1f482634fd6bf9c14e14010d956b0d725425
Andreas Pauley <andreas@pauley.org.za>
2016-05-25 17:52:16 +0200

    usage

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,17 @@
 module Main where
 
-import System.Environment (getArgs)
+import System.Environment (getArgs, getProgName)
 import Lib
 
 main :: IO ()
 main = do
-  _exitCode <- shellDate
+  exit <- shellDate
+  me   <- getProgName
   args <- getArgs
   let (repoPath, sleepSeconds) = parseArgs args
+
+  putStrLn $ usage me
   putStrLn $ "Fetching every " ++ show sleepSeconds ++ " seconds in " ++ show repoPath
+
+usage :: String -> String
+usage me  = "Usage: " ++ me ++ " /path/to/repo sleepSeconds"
