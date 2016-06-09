c35b1b5d437a046b5f6177e6adeb9b97b25d1086
Andreas Pauley <andreas@pauley.org.za>
2016-06-09 23:41:42 +0200

    Move everything to Main.hs until further notice

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,10 @@ module Main where
 
 import Turtle
 import Prelude hiding (FilePath, log)
-import Lib
+import Filesystem.Path.CurrentOS (fromText)
+import Data.Text (pack)
+
+defaultSleepSeconds = 120
 
 main :: IO ()
 main = do
@@ -16,3 +19,36 @@ main = do
 parser :: Parser (FilePath, Int)
 parser = (,) <$> argPath "repo"  "The path to a git repository"
              <*> argInt "sleepSeconds" "The number of seconds to sleep between fetches"
+
+fetch :: FilePath -> NominalDiffTime -> IO ()
+fetch repoDir sleepSeconds = do
+  cd repoDir
+  dir <- pwd
+
+  fetchOne dir .||. die (format ("Error fetching in "%fp%". Is this a git repository?") dir)
+
+  log $ format ("Fetching every "%s%" in " %fp% "") (pack $ show sleepSeconds) dir
+  fetchEvery dir sleepSeconds
+
+fetchOne :: FilePath -> IO ExitCode
+fetchOne dir = do
+  log $ format ("Running a git fetch in " %fp) dir
+  exitCode <- shell "git fetch" empty
+  log $ format ("Done running a git fetch in " %fp% " - "%s%"\n") dir $ pack $ show exitCode
+  return exitCode
+
+fetchEvery :: FilePath -> NominalDiffTime -> IO ()
+fetchEvery dir sleepSeconds = do
+  sleep sleepSeconds
+  fetchOne dir
+  fetchEvery dir sleepSeconds
+
+log :: Text -> IO ()
+log msg = do
+  now <- dateString
+  echo $ format (s%" - "%s) now msg
+
+dateString :: IO Text
+dateString = do
+  d <- date
+  return $ pack $ show d
